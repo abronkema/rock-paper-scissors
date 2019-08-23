0905b9a83a596a9c35ee5d8fdeb334d29d724a74
Anthony Bronkema <abronkema@customink.com>
2019-08-23 13:28:36 -0400

    adds game play, end, scoring functionality and reset button

--- app.js
+++ app.js
@@ -1,109 +1,103 @@
-let computerScore, playerScore;
+let computerScore = 0;
+let playerScore = 0;
 const playerPicksDisplaySection = document.getElementById("player-pick");
 const computerPicksDisplaySection = document.getElementById("computer-pick");
 const resultsMessageDisplaySection = document.getElementById("results-message");
 const scoreDisplaySection = document.getElementById("score");
 const playerSelectionButtons = document.querySelectorAll("button");
 
+scoreDisplaySection.innerHTML = ` ${playerScore} - ${computerScore}`;
+
 let options = ["rock", "paper", "scissors"];
 function computerPlay() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function resultMessage(result, playerSelection, computerSelection) {
-  if (result == "win") {
-    return `
+function resultMessage(result) {
+  let resultMessageText = "";
+  if (result == "game over") {
+    console.log("winner");
+  } else if (result == "win") {
+    resultMessageText = `
     You won this round!
     `;
   } else if (result == "loss") {
-    return `
+    resultMessageText = `
     You lost this round!
     `;
   } else {
-    return `
+    resultMessageText = `
     That round was a draw.
     `;
   }
+  resultsMessageDisplaySection.innerHTML = `${resultMessageText}`;
+}
+
+function updateScoreboard(winner) {
+  if (winner == "player") {
+    playerScore++;
+  } else if (winner == "computer") {
+    computerScore++;
+  }
+  scoreDisplaySection.innerText = `${playerScore} - ${computerScore}`;
+}
+
+function checkForWinner() {
+  if (playerScore == 5 || computerScore == 5) {
+    declareWinner();
+    // } else {
+    //   resultMessage(result);
+  }
+}
+
+function declareWinner() {
+  let winnerName;
+  if (playerScore > computerScore) {
+    winnerName = `Player`;
+  } else {
+    winnerName = `Computer`;
+  }
+  resultsMessageDisplaySection.innerHTML = `Game Over. ${winnerName.toUpperCase()} wins!`;
+}
+
+function resetGame() {
+  playerPicksDisplaySection.innerHTML = "";
+  computerPicksDisplaySection.innerHTML = "";
+  resultsMessageDisplaySection.innerHTML = "";
+  computerScore = 0;
+  playerScore = 0;
+  updateScoreboard();
 }
 
 function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay();
   playerPicksDisplaySection.innerHTML = playerSelection;
   computerPicksDisplaySection.innerHTML = computerSelection;
-  if (playerSelection == computerSelection) {
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "tie",
-      playerSelection,
-      computerSelection
-    ));
+  if (playerSelection == "reset") {
+    resetGame();
+  } else if (playerSelection == computerSelection) {
+    resultMessage("tie");
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    computerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "loss",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("computer");
+    resultMessage("loss");
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    playerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "win",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("player");
+    resultMessage("win");
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    computerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "loss",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("computer");
+    resultMessage("loss");
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    playerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "win",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("player");
+    resultMessage("win");
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    computerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "loss",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("computer");
+    resultMessage("loss");
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    playerScore++;
-    return (resultsMessageDisplaySection.innerHTML = resultMessage(
-      "win",
-      playerSelection,
-      computerSelection
-    ));
+    updateScoreboard("player");
+    resultMessage("win");
   }
-  console.log(`Computer: ${computerScore}. Player: ${playerScore}`);
+  checkForWinner(playerScore, computerScore);
 }
-function game() {
-  let round = 1;
-  computerScore = 0;
-  playerScore = 0;
-  // while (round <= 5) {
-  // let computerSelection = computerPlay();
-  // let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-  console.log(playRound(playerSelection, computerSelection));
-  round++;
-  // }
-  if (playerScore > computerScore) {
-    console.log(`You won! Player: ${playerScore}. Computer: ${computerScore}.`);
-  } else if (playerScore == computerScore) {
-    console.log(
-      `It's a tie! Player: ${playerScore}. Computer: ${computerScore}.`
-    );
-  } else {
-    console.log(
-      `You lost. Player: ${playerScore}. Computer: ${computerScore}.`
-    );
-  }
-}
-
 playerSelectionButtons.forEach(function(button) {
   button.addEventListener("click", function(el) {
     playRound(el.target.innerHTML.toLowerCase());
